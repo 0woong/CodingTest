bc52f917b5587db1d9334a865d68e41fe0a3ad84
Eroe <89058193+0woong@users.noreply.github.com>
2022-08-16 16:21:11 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+ public class Main {
+     public static void main(String[] args) throws IOException {
+         Scanner scanner = new Scanner(System.in);
+         int[] cnt = new int[6];
+         int[] full = new int[]{1, 1, 2, 2, 2, 8};
+         for (int i = 0; i < cnt.length; i++) {
+             cnt[i] = scanner.nextInt();
+         }
+         for (int i = 0; i < full.length; i++) {
+             cnt[i] = full[i] - cnt[i];
+             System.out.println(cnt[i]);
+         }
+     }
+ }
