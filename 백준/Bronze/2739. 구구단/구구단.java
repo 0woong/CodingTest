54484ddfd4b451aad17bf406b5c7dad605fb0f6d
Eroe <89058193+0woong@users.noreply.github.com>
2023-11-22 17:20:31 +0900

    [Bronze V] Title: 구구단, Time: 152 ms, Memory: 16096 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Main{
+    public static void main(String[] arg) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=1;i<10;i++){
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+    }
+}
