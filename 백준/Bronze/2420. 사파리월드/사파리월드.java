09df8a36054e7ea358833a14ad34778e992d1759
Eroe <89058193+0woong@users.noreply.github.com>
2023-11-28 05:32:00 +0900

    [Bronze V] Title: 사파리월드, Time: 132 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+        long sum = Math.abs(a-b);
+        bw.write(sum+"");
+        bw.flush();
+        bw.close();
+    }
+}
