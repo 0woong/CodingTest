e7dbd1ae9a9aff7a281252f2450e4d8e63d1f0aa
Eroe <89058193+0woong@users.noreply.github.com>
2023-11-28 04:01:58 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,45 +1,38 @@
 import java.io.*;
-
-public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String str = br.readLine();
-
-		int len = str.length();
-		int count = 0;
-
-		for (int i = 0; i < len; i++) {
-			char ch = str.charAt(i);
-			if (ch == 'c' && i < len - 1) {
-				if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {
-					i++;
-				}
-			}
-			else if (ch == 'd' && i < len - 1) {
-				if (str.charAt(i + 1) == '-') {
-					i++;
-				} else if (str.charAt(i + 1) == 'z' && i < len - 2) {
-					if (str.charAt(i + 2) == '=') {
-						i += 2;
-					}
-				}
-			}
-			else if ((ch == 'l' || ch == 'n') && i < len - 1) {
-				if (str.charAt(i + 1) == 'j') {
-					i++;
-				}
-			}
-			else if ((ch == 's' || ch == 'z') && i < len - 1) {
-				if (str.charAt(i + 1) == '=') {
-					i++;
-				}
-			}
-			count++;
-		}
-		bw.write(count + "");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+        int n = s.length();
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='c' && i<n-1){
+                if(s.charAt(i+1)=='=' || s.charAt(i+1)=='-'){
+                    i++;
+                }
+            } else if(s.charAt(i)=='d' && i<n-1){
+                if(s.charAt(i+1)=='z' && i<n-2){
+                    if(s.charAt(i+2)=='='){
+                        i+=2;
+                    }
+                } else if(s.charAt(i+1)=='-') {
+                    i++;
+                }
+            } else if((s.charAt(i)=='l' || s.charAt(i)=='n') && i<n-1){
+                if(s.charAt(i+1)=='j'){
+                    i++;
+                }
+            } else if((s.charAt(i)=='s' || s.charAt(i)=='z') && i<n-1){
+                if(s.charAt(i+1)=='='){
+                    i++;
+                }
+            }
+            count++;
+        }
+        bw.write(count+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
