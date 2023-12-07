422d144cf950e7ba82abe74854b4e1890721e590
Eroe <89058193+0woong@users.noreply.github.com>
2023-12-07 20:01:30 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14244 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int cal1 = a+b;
+        int cal2 = a-b;
+        bw.write(cal1*cal2+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
