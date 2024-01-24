bcae8f74dcb64f519f3d4b488e2867164e82265a
Eroe <89058193+0woong@users.noreply.github.com>
2024-01-24 16:58:30 +0900

    [Silver IV] Title: 다음 소수, Time: 400 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -1,34 +1,35 @@
 import java.io.*;
-public class Main{
-    public static void main(String[] args) throws Exception{
+public class Main{    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
         for(int i=0;i<n;i++){
             long m = Long.parseLong(br.readLine());
-            if(m<=2) {
-            	sb.append(2).append("\n");
-            	continue;
-            }
-            while(true) {
-            	boolean flag = true;
-            	for(int j=2;j<Math.sqrt(m)+1;j++) {
-            		if(m%j==0) {
-            			flag = false;
-            			break;
-            		}
-            	}
-            	if(flag) {
-            		sb.append(m).append("\n");
-            		break;
-            	}
-            	m++;
+            while(true){
+                if(sosu(m) == 1){
+                    bw.write(m+"\n");
+                    break;
+                }
+                m++;
             }
         }
-        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
+    }
+    
+    public static long sosu(long a){
+        if(a<2){
+            return 0;
+        } else{
+            for(long i=2;i<=(long)Math.sqrt(a);i++){
+                if(a%i==0){
+                	return 0;
+                }
+            }
+            return 1;
+        }
     }
 }
