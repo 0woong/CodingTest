017060f512452521f11aeb223edcdd7ddce53c3f
Eroe <89058193+0woong@users.noreply.github.com>
2024-02-27 16:40:58 +0900

    [Silver II] Title: 마인크래프트, Time: 684 ms, Memory: 36516 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -4,44 +4,44 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int[][] arr = new int[N][M];
-		int min = 256;
-		int max = 0;
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < M; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				if(min > arr[i][j]) min = arr[i][j];
-				if(max < arr[i][j]) max = arr[i][j];
-			}
-		}
-		int time = 99999999;
-		int high = 0;
-		for(int i = min; i <= max; i++) {
-			int count = 0;
-			int block = B;
-			for(int j = 0; j < N; j++) {
-				for(int k = 0; k < M; k++) {
-					if(i < arr[j][k]) {
-						count += ((arr[j][k] - i) * 2);
-						block += (arr[j][k] - i);
-					}else {
-						count += (i - arr[j][k]);
-						block -= (i - arr[j][k]);
-					}
-				}
-			}
-			if(block < 0) break;
-			if(time >= count) {
-				time = count;
-				high = i;
-			}
-		}
-		bw.write(time + " " + high);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+        int min = 256;
+        int max = 0;
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<M;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(min>arr[i][j]) min = arr[i][j];
+                if(max<arr[i][j]) max = arr[i][j];
+            }
+        }
+        int maxtime = Integer.MAX_VALUE;
+        int high = 0;
+        for(int i=min;i<=max;i++){
+            int time = 0;
+            int block = B;
+            for(int j=0;j<N;j++){
+                for(int k=0;k<M;k++){
+                    if(i<arr[j][k]){
+                        time += (arr[j][k]-i)*2;
+                        block += arr[j][k]-i;
+                    } else{
+                        time += i-arr[j][k];
+                        block -= i-arr[j][k];
+                    }
+                }
+            }
+            if(block<0) break;
+            if(maxtime>=time){
+                maxtime = time;
+                high = i;
+            }
+        }
+        bw.write(maxtime+" "+high);
         bw.flush();
         bw.close();
         br.close();
