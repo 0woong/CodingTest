6fbd195128d04c2ce8cdf3c31046a36607b25ee9
Eroe <89058193+0woong@users.noreply.github.com>
2024-02-28 23:22:09 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,36 +1,34 @@
 import java.io.*;
 import java.util.Stack;
 public class Main{
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        
-        for(int i=0;i<n;i++){
-            sb.append(result(br.readLine())).append("\n");
+        int T = Integer.parseInt(br.readLine());
+        while(T-->0){
+            String s = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            boolean flag = true;
+            for(int i=0;i<s.length();i++){
+                char c = s.charAt(i);
+                if(c=='('){
+                    stack.push(c);
+                }else if(stack.isEmpty()){
+                    flag = false;
+                } else{
+                    stack.pop();
+                }
+            }
+            if(stack.isEmpty() && flag){
+                sb.append("YES\n");
+            } else{
+                sb.append("NO\n");
+            }
         }
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
-    }
-    public static String result(String s){
-        Stack<Character> stack = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c=='('){
-                stack.push(c);
-            } else if(stack.empty()){
-                return "NO";
-            } else{
-                stack.pop();
-            }
-        }
-        if(stack.empty()){
-            return "YES";
-        } else{
-            return "NO";
-        }
     }
 }
