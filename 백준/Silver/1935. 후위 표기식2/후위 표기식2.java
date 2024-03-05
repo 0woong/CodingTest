494c1cd536bab23496893db1e2e962a7ed12a469
Eroe <89058193+0woong@users.noreply.github.com>
2024-03-05 22:31:53 +0900

    [Silver III] Title: 후위 표기식2, Time: 132 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.Stack;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        String data = br.readLine();
+        double[] arr = new double[N];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = Double.parseDouble(br.readLine());
+        }
+        Stack<Double> stack = new Stack<>();
+        double result = 0;
+        for(int i = 0; i < data.length(); i++) {
+            if('A' <= data.charAt(i) && data.charAt(i) <= 'Z') {
+                stack.push(arr[data.charAt(i) - 'A']);
+            } else {
+                if(!stack.isEmpty()) {
+                    double first = stack.pop();
+                    double second = stack.pop();
+                    switch (data.charAt(i)) {
+                        case '+':
+                            result = second + first;
+                            stack.push(result);
+                            continue;
+                        case '-':
+                            result = second - first;
+                            stack.push(result);
+                            continue;
+                        case '*':
+                            result = second * first;
+                            stack.push(result);
+                            continue;
+                        case '/':
+                            result = second / first;
+                            stack.push(result);
+                            continue;
+                    }
+                }
+            }
+        }
+        bw.write(String.format("%.2f", stack.pop()));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
