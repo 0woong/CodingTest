a24f54fec2c5b842a6451ef65f0a9c2d27d16b98
Eroe <89058193+0woong@users.noreply.github.com>
2024-03-09 10:51:12 +0900

    [Bronze III] Title: 네 수, Time: 132 ms, Memory: 14460 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b = st.nextToken();
+        long first = Long.parseLong(a+b);
+        String c = st.nextToken();
+        String d = st.nextToken();
+        long second = Long.parseLong(c+d);
+        bw.write(first+second+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
