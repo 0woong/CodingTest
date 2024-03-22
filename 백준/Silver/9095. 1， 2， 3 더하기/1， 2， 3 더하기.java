2f29238025c93273148727158514d66833eb2370
Eroe <89058193+0woong@users.noreply.github.com>
2024-03-22 21:10:33 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,11 +1,11 @@
 import java.io.*;
 public class Main{
-    static int dp[] = new int [11];
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        int dp[] = new int [11];
         dp[1] = 1;
 		dp[2] = 2;
 		dp[3] = 4;
